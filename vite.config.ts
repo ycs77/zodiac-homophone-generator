9c107fa66d8bb85fb42b4140655bbde50699ca4f
Lucas Yang <yangchenshin77@gmail.com>
2024-09-28 19:47:10 +0800

    Code spliting for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -33,4 +33,17 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/phaser')) {
+            return 'phaser'
+          } else if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
